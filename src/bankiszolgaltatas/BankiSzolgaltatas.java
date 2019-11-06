415a280b3e4da1c2f4094b1a38825cbde549bd9a
Diak <Diak@10.4.117.8>
2019-11-06 14:24:59 +0100

    Szamla

---
+++ src/bankiszolgaltatas/BankiSzolgaltatas.java
@@ -0,0 +1,18 @@
+
+package bankiszolgaltatas;
+
+import tulajdonos.Tulajdonos;
+
+public abstract class BankiSzolgaltatas {
+    
+    private Tulajdonos tulajdonos;
+
+    public BankiSzolgaltatas(Tulajdonos tulajdonos) {
+        this.tulajdonos = tulajdonos;
+    }
+    
+    public Tulajdonos getTulajdonos() {
+        return tulajdonos;
+    }
+    
+}
--- src/tulajdonos/Tulajdonos.java
+++ src/tulajdonos/Tulajdonos.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tulajdonos;
 
-/**
- *
- * @author Diak
- */
-public abstract class Tulajdonos {
+public final class Tulajdonos {
     private String nev;
 
     public Tulajdonos(String nev) {
